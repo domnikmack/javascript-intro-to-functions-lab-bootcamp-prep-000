8160fcfa24f8f8afca46bc3836893212d4797e2e
Domnik Mack <domnikmack@gmail.com>
2017-09-19 01:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase());
 }
+funtion whiperShout(string) {
+  console.log(string.toLowerCase));
+}
+function sayHiToGrandma(string) {
+  if (string.toLowerCase===string) {
+    return "I can\'t hear you!";
+  } else if (string.toUpperCawe===string) {
+    return "YES INDEED!";
+  } else if (string==="I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
